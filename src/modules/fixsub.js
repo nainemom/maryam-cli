5a389d93471e250ef1f6df8ef8dba58b5a1c90ed
nainemom <nainemom@gmail.com>
2017-05-07 18:11:33 +0430

    strip html tags

--- src/modules/fixsub.js
+++ src/modules/fixsub.js
@@ -7,7 +7,7 @@ module.exports = function (src = null, dst = null) {
   try {
     const content = fs.readFileSync(src);
     const iconv = new Iconv.Iconv('WINDOWS-1256', 'UTF-8');
-    const result = iconv.convert(content).toString('utf8');
+    const result = iconv.convert(content).toString('utf8').replace(/<\/?[^>]+(>|$)/g, "");
     const saveDir = dst ? dst : src;
     fs.writeFileSync(saveDir, result, 'UTF-8');
     helper.spinner(false);
