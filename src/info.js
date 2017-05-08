6f4c85374c080464db0bb421378faa8830172a4b
nainemom <nainemom@gmail.com>
2017-05-08 13:16:40 +0430

    fix help.js importing

--- src/info.js
+++ src/info.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 const pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '../package.json')));
-const help = fs.readFileSync(path.join(__dirname, 'help.js'), { encoding: 'utf8' });
+const help = require(path.join(__dirname, 'help.js'));
 
 module.exports = {
   version: pkg.version,
