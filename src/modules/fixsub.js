8e5dd25de35cb3dae998add00ffd47e9d84e58ff
Amir Momenian <nainemom@gmail.com>
2020-11-30 14:45:10 +0330

    use cwd to resolve paths

--- src/modules/fixsub.js
+++ src/modules/fixsub.js
@@ -1,14 +1,16 @@
 import path from 'path';
-const helper = require(path.join(__dirname, '../helper.js'));
+import process from 'process';
 import fs from 'fs';
 import Iconv from 'iconv';
+const helper = require(path.join(__dirname, '../helper.js'));
 module.exports = function (src = null, dst = null) {
   helper.spinner(true);
+  const cwd = process.cwd();
   try {
-    const content = fs.readFileSync(src);
+    const content = fs.readFileSync(path.resolve(cwd, src));
     const iconv = new Iconv.Iconv('WINDOWS-1256', 'UTF-8');
     const result = iconv.convert(content).toString('utf8').replace(/<\/?[^>]+(>|$)/g, "");
-    const saveDir = dst ? dst : src;
+    const saveDir = path.resolve(cwd, dst || src);
     fs.writeFileSync(saveDir, result, 'UTF-8');
     helper.spinner(false);
     helper.ok(`File '${saveDir}' saved!`);
